0dfeefb446ae7fb7faa2d85cc090d7ff97cf10ca
Junior DevLeague <jason@sudokrew.com>
2016-12-27 16:57:34 -1000

    javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-function getbackgroundcolor(color)
+function getBackgroundColor(color){
+ 
+}
+
+ function changeColorOnClick(idName){
+ 	var originalElementColor=document.getElementById("selectedColorName").style.backgroundColor;
+	document.getElementById(idName).style.backgroundColor = originalElementColor
+
+ 	
+ }
